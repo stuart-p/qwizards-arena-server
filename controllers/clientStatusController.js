b212323533f22c8fe95a213ce32b6d504e43bd1d
Dan <57401847+DvnCodes@users.noreply.github.com>
2020-03-20 09:21:21 +0000

    Update clientStatusController.js

--- controllers/clientStatusController.js
+++ controllers/clientStatusController.js
@@ -1,7 +1,7 @@
 const clientList = {};
 let lobbyList = [];
 let gameCount = 1;
-const maxPlayers = 4;
+const maxPlayers = 3;
 let currentGame = 1;
 const playersWaitingForQuiz = {};
 const { fetchQuestions } = require("../models/quiz.models");
